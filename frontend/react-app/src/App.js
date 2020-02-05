81fbba1ae403a947bc923060f14a63100004d4cb
PrakharGoenka <prakhar.goinka@gmail.com>
2020-02-05 14:51:48 +0530

    frontend: Add routes, configurations

--- frontend/react-app/src/App.js
+++ frontend/react-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Homepage from './components/Homepage'
 
 function App() {
   return (
-    <div>
-      <h3> Hello World!!!</h3>
-    </div>
+    <Router>
+				<Switch>
+					<Route exact path="/" component={Homepage} />
+				</Switch>
+			</Router>
   );
 }
 
