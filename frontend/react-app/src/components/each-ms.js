45d91b1b7092ba766f5df672689420fdbe8efefe
Prakhar Gupta <prakhargupt00@gmail.com>
2020-04-14 08:12:00 +0530

    Frontend: remove comments

--- frontend/react-app/src/components/each-ms.js
+++ frontend/react-app/src/components/each-ms.js
@@ -1,23 +1,7 @@
-/***********************************************************************************************************************************************
- A GUIDE TO CREATE A MICRO-FRONTEND
- 
- This micro-frontend is based on ReactJS components
- Micro-frontends are counterparts of microservices in the frontend-world, they are lightweight, can be developed independently by a team and be 
- deployed on it's own. They are portable ie. can be embedded in any supported UI.
- Note: This example is just a representation of a micro-frontend, on how should it be coded using react and how we link it to microservices
- This micro-frontend is used to display the details of a microservice owned by a organization. In the final application, this micro-frontend is 
- embedded multiple times, at multiple places, each for a microservice whose details need to be displayed.
- **********************************************************************************************************************************************/
-
-
 import React , {Component} from 'react'
 import {Link} from 'react-router-dom';
 import './view_ms.css'
 import axios from 'axios'
-
-/* 
-The micro-frontend in this example is shown as a React component that can be embedded in other components
-*/
 
 class Each_MS extends Component{
   state={
@@ -26,23 +10,14 @@ class Each_MS extends Component{
   }
 
   componentWillMount(){
-    /*
-    Micro-frontends usually recieve a part or whole of their information from the UI/component it is embedded in. In this case it recieves the _id 
-    of the microsrvice whose details it is rendering. This is present in this.props._id.
-    It then gets the whole details by making a request to a microservice in the backend that manages the details of all the microservices present owned 
-    by the organization
-    The micro-frontend stores the data returned by the micro-service in it's state so that it can be rendered 
-    */
+    
     axios.post("http://localhost:8080/existing/one",{micro_id:this.props.micro_id}).then(res => {
       console.log("Data reached")
       this.setState({micro:res.data.micro,loadStatus:true})
     })
   }
   render(){
-    /*
-    This function is returns the content that is to displyed by the micro-frontend.
-    The data that was requested from the backend microservice and saved in the state is structured in the form of HTML.
-    */
+   
     var micro = this.state.micro ;
     var keywords = micro.keywords ;
     var tech_stack = micro.tech_stack ;
@@ -85,13 +60,8 @@ class Each_MS extends Component{
         </div>
     )
   }
-  /*
-  Sometimes, micro-frontends listen to some events and perform actions as well, the function(s) present below are normal event-handlers from react.
-  */
+ 
 
 }
 
-/*
-Component here is exported so that it can be embedded in other , larger Components
-*/
 export default Each_MS
