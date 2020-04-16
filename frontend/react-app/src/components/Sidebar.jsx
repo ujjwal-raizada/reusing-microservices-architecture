821422c8960621ff5a6d649157851832d2909c89
Prakhar Gupta <prakhargupt00@gmail.com>
2020-04-16 13:05:06 +0530

    frontend: Update existing route

--- frontend/react-app/src/components/Sidebar.jsx
+++ frontend/react-app/src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ class Sidebar extends Component {
      }
 
     componentWillMount(){
-        axios.get('http://localhost:8080/requested/all').then(res=>{
+        axios.get('http://localhost:8080/existing/all').then(res=>{
           this.setState({micros:res.data.micros,loadStatus:true});
           console.log(res.data.micros);
         })
