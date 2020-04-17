128cfcae85cacd79c46f099203bb5b90200942eb
Ujjwal Raizada <ujjwalraizada@gmail.com>
2020-04-16 19:15:37 -0700

    Service A: Add bulk import endpoint

--- existing-microservice/server.py
+++ existing-microservice/server.py
@@ -11,42 +11,56 @@ users = {
 }
 
 posts = []
+persistant_status = False
 
-@app.route('/')
+
+def persistant(function):
+    def wrapper():
+        global posts
+        global persistant_status
+        if (not persistant_status):
+            with open("user_data", "rb+") as file:
+                posts = pickle.load(file)
+            persistant_status = True
+
+        return_val = function()
+        with open("user_data", "wb+") as file:
+                pickle.dump(posts, file)
+        return return_val
+    return wrapper
+
+        
+
+@app.route('/', endpoint='index')
 def index():
     text = """
-    Welcome to home page of test API. Sent POST requests to /api
-    
-    endpoint accepts -
-    GET - displays the posts. goto <a href="/api">GET /api</a> to check.
-    POST - Send POST requests to create posts in JSON format.
+Welcome to home page of test API. Sent POST requests to /api
 
-        * four parameters required in POST request.
-            1. username
-            2. password
-            3. subject
-            4. text
+endpoint accepts -
+GET - displays the posts. goto <a href="/api">GET /api</a> to check.
+POST - Send POST requests to create posts in JSON format.
 
-        * example users
-            1. UjjwalRaizada Ujjwal123
-            2. PrakharGoenka Prakhar123
+    * four parameters required in POST request.
+        1. username
+        2. password
+        3. subject
+        4. text
+
+    * example users
+        1. UjjwalRaizada Ujjwal123
+        2. PrakharGoenka Prakhar123
 
     """
     return text.replace('\n', '<br>').replace("    ", "&emsp;")
 
 
-@app.route('/api', methods=['GET', 'POST'])
+@app.route('/api', methods=['GET', 'POST'], endpoint='api')
+@persistant
 def api():
-    global posts
-    with open("user_data", "rb+") as file:
-        posts = pickle.load(file)
 
     if request.method == 'POST':
 
-        
-
         req_data = request.get_json()
-        print(req_data)
 
         username = req_data['username']
         password = req_data['password']
@@ -56,9 +70,6 @@ def api():
         if ((username in users) and users[username] == password):
             print("user check passed...")
             posts.append((username, post_subject, post_text))
-
-            with open("user_data", "wb+") as file:
-                pickle.dump(posts, file)
 
             return json.dumps({"status": "success"})
         else:
@@ -73,6 +84,24 @@ def api():
         return output
 
 
+@app.route('/api/bulk', methods=['POST'], endpoint='bulk-api')
+@persistant
+def bulk_api():
+    req_data = request.get_json()
+
+    for data in req_data:
+        username = data['username']
+        password = data['password']
+        post_subject = data['subject']
+        post_text = data['text']
+
+        if ((username in users) and users[username] == password):
+            print("user check passed...")
+            posts.append((username, post_subject, post_text))
+        else:
+            print("user check failed...")
+            return json.dumps({"status": "failed"})
+    return json.dumps({"status": "success"})
 
 
 if __name__ == '__main__':
--- existing-microservice/testing_server.py
+++ existing-microservice/testing_server.py
@@ -1,12 +1,29 @@
 import requests
+from copy import deepcopy
+import random
 
-URL = "http://ujjwalraizada.pythonanywhere.com/api"
+URL1 = "http://ujjwalraizada.pythonanywhere.com/api"
+URL2 = "http://localhost:5000/api"
 data = {}
 data["username"] = "UjjwalRaizada"
 data["password"] = "Ujjwal123"
 data["subject"] = "universe"
 data["text"] = "universe is large"
 
-r = requests.post(URL, json=data)
-print(r)
-print(r.text)
+
+def test_api():
+    r = requests.post(URL2, json=data)
+    print(r)
+    print(r.text)
+
+def test_bulk():
+    req_data = []
+    for i in range(random.randint(1, 11)):
+        temp_data = deepcopy(data)
+        temp_data["text"] += " - {}".format(i)
+        req_data.append(temp_data)
+    r = requests.post(URL2 + "/bulk", json=req_data)
+    print(r)
+    print(r.text)
+
+test_bulk()
