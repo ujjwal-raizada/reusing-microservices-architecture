98fa70a0be8e0b28067ddafa3a4a6c717f68afcf
PrakharGoenka <prakhar.goinka@gmail.com>
2020-04-21 19:21:15 +0530

    frontend: Update homepage

--- frontend/react-app/src/components/Home.jsx
+++ frontend/react-app/src/components/Home.jsx
@@ -1,73 +1,25 @@
-import React, { Component } from 'react';
-import './home.css'
+import React, { Component,Fragment } from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar' ;
-import Rightbar from './Rightbar';
-import Each_MS from './each-ms';
-import Each_MSB from './each-msB';
+import {Row, Col} from 'react-bootstrap'
+
 
 class Home extends Component {
-    state = {  
-      hasMicroCall: false, 
-      hasMicroCallB: false, 
-      requestedMicroA: null,
-      requestedMicroB: null
-    };
-
-    renderMicroservice = micro_id =>{
-      console.log('Call for microservice!!');
-      this.setState({hasMicroCall:true, requestedMicroA:micro_id}) ; 
-    };
-
-    renderMicroserviceB = micro_id =>{
-      console.log('Call for microserviceB!!');
-      this.setState({hasMicroCallB:true, requestedMicroB:micro_id}) ; 
-    };
-
-    showA = () =>{
-      if(this.state.requestedMicroA !=null && this.state.hasMicroCall)
-      return <Each_MS key= {this.state.requestedMicroA} micro_id={this.state.requestedMicroA} link={false}/>;
-    }
-
-    showB = () =>{
-      if(this.state.requestedMicroB !=null && this.state.hasMicroCallB)
-      return <Each_MSB key= {this.state.requestedMicroB} micro_id={this.state.requestedMicroB} link={false}/>;
-    }
-    render() { 
-      if(this.state.hasMicroCall === false && this.state.hasMicroCallB === false){
-        return ( 
-          <div className="home-wrap">
-              <Header/>
-              <div className="landing">
-                <h1 className="home-title">MicroDeveloper</h1>
-                <div className="land-desc col-lg-6 col-md-7">
-                  Micro Developer is a platform for reusing existing microservices to create new microservies.
-                </div>
-                <Sidebar  renderMicroservice = {this.renderMicroservice}/>
-                <Rightbar renderMicroserviceB = {this.renderMicroserviceB}/>
-            </div>
-          </div> 
-        );
-      }else{
-        return ( 
-        <div className="home-wrap">
-              <Header/>
-              <div className="landing">
-                {/* <h1 className="home-title">MicroDeveloper</h1>
-                <div className="land-desc col-lg-6 col-md-7">
-                  Micro Developer is a platform for reusing existing microservices to create new microservies.
-                </div> */}
-                <Sidebar renderMicroservice = {this.renderMicroservice}/>
-                <div className ="show-micro">
-                  {this.showA()}
-                  {this.showB()}
-                </div>
-                <Rightbar renderMicroserviceB = {this.renderMicroserviceB}/>
-            </div>
-          </div> 
-        );
-      }
-    }
+  render() { 
+    return (
+      <Fragment>
+        <Header/>
+        <Row >
+          <Col xs={2}>
+            <Sidebar/>
+          </Col>
+          <Col>
+            <h2 className="text-center"> Welcome to the <br/> Portal!!!</h2>
+          </Col>    
+        </Row>
+      </Fragment>        
+    )
+  }
 }
 
 export default Home;
