054ae0c52eb8e501f10ffbf37ea9e9c84da3ce08
PrakharGoenka <prakhar.goinka@gmail.com>
2020-04-21 19:21:15 +0530

    frontend: Update homepage

--- frontend/react-app/src/index.js
+++ frontend/react-app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './index.css';
 import InitConfig from "./InitConfig";
 
 InitConfig(false);
