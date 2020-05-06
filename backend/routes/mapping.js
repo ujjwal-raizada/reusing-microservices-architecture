d7b754703dbc8fdf0e041766210a9c7a252cfc5a
Ujjwal Raizada <ujjwalraizada@gmail.com>
2020-05-06 05:42:32 +0530

    routes: Add routes for transformation and template

--- backend/routes/mapping.js
+++ backend/routes/mapping.js
@@ -6,10 +6,21 @@ const getAllController = require(path + '/get-all')
 const getTemplatesController = require(path + '/get-templates')
 const getMappingController = require(path + '/get-mapping')
 const updateMappingController = require(path + '/update-mapping')
+const TemplateController = require(path + '/template')
+const TransformationController = require(path + '/transformation')
 
 router.get('/all', getAllController)
 router.get('/templates', getTemplatesController)
 router.post('/get', getMappingController)
 router.post('/update', updateMappingController)
- 
+
+router.get('/transformations/all', TransformationController.all);
+router.get('/transformations/find/:name', TransformationController.find);
+router.get('/transformations/:name/templates', TransformationController.getAllTemplates);
+router.post('/transformations/create', TransformationController.create)
+
+router.get('/templates/all', TemplateController.all);
+router.get('/templates/find/:name', TemplateController.find);
+router.post('/templates/create', TemplateController.create)
+
 module.exports = router
