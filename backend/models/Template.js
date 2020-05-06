0e4a0cc86f2b4dee88796a058aa898b10fcd847e
Ujjwal Raizada <ujjwalraizada@gmail.com>
2020-05-06 05:40:38 +0530

    model: Add template model

---
+++ backend/models/Template.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+var TemplateSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    code: {
+        type: String,
+        required: true
+    },
+    mapping: {
+        type: Schema.Types.ObjectId,
+        ref: "Transformation"
+    }
+});
+
+var TemplateModel = mongoose.model("Template", TemplateSchema);
+
+module.exports = TemplateModel; 
+ 
